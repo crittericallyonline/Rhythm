c08863240381f9ff63635097ccc8c4080162543b
tennyballs <player130games@gmail.com>
2025-12-08 19:29:49 -0800

    PLEASE REMOVE HTML

--- script/main.js
+++ script/main.js
@@ -1,53 +1,93 @@
 "use strict";
-var KEYBOARDMANAGER;
-(function (KEYBOARDMANAGER) {
-    const keys = new Uint32Array(4);
-    function down(ev) {
-        ev.preventDefault();
-        console.log(ev.code);
-    }
-    function up(ev) {
-        ev.preventDefault();
-    }
-    document.addEventListener('keydown', down, {
-        passive: false,
-        capture: false,
-        once: false
-    });
-    document.addEventListener('keyup', up, {
-        passive: false,
-        capture: false,
-        once: false
-    });
-})(KEYBOARDMANAGER || (KEYBOARDMANAGER = {}));
-var FILEMANAGER;
-(function (FILEMANAGER) {
-    function download(fileData, fileName) {
-        const e = document.createElement('a');
-        e.setAttribute('href', `data:text/plain;charset=utf-8,${fileData}`);
-        e.setAttribute('download', fileName);
-        e.style.display = 'none';
-        document.body.appendChild(e);
-        e.click();
-        e.remove();
-    }
-    FILEMANAGER.download = download;
-})(FILEMANAGER || (FILEMANAGER = {}));
-var Players;
-(function (Players) {
-    let noteWidth = 75;
-    let noteHeight = 75;
-    class Player {
-        constructor() {
+var Mods;
+(function (Mods) {
+    Mods[Mods["xmod"] = 0] = "xmod";
+    Mods[Mods["x"] = 1] = "x";
+    Mods[Mods["y"] = 2] = "y";
+    Mods[Mods["z"] = 3] = "z";
+    Mods[Mods["movex"] = 4] = "movex";
+    Mods[Mods["movey"] = 5] = "movey";
+    Mods[Mods["movez"] = 6] = "movez";
+    Mods[Mods["rotationx"] = 7] = "rotationx";
+    Mods[Mods["rotationy"] = 8] = "rotationy";
+    Mods[Mods["rotationz"] = 9] = "rotationz";
+    Mods[Mods["skewx"] = 10] = "skewx";
+    Mods[Mods["skewy"] = 11] = "skewy";
+    Mods[Mods["drunk"] = 12] = "drunk";
+    Mods[Mods["drunkspeed"] = 13] = "drunkspeed";
+    Mods[Mods["drunkoffset"] = 14] = "drunkoffset";
+    Mods[Mods["drunkperiod"] = 15] = "drunkperiod";
+    Mods[Mods["tipsy"] = 16] = "tipsy";
+    Mods[Mods["tipsyspeed"] = 17] = "tipsyspeed";
+    Mods[Mods["tipsyoffset"] = 18] = "tipsyoffset";
+    Mods[Mods["tipsyperiod"] = 19] = "tipsyperiod";
+    Mods[Mods["drunkz"] = 20] = "drunkz";
+    Mods[Mods["drunkzspeed"] = 21] = "drunkzspeed";
+    Mods[Mods["drunkzoffset"] = 22] = "drunkzoffset";
+    Mods[Mods["drunkzperiod"] = 23] = "drunkzperiod";
+})(Mods || (Mods = {}));
+var Game;
+(function (Game) {
+    const container = document.getElementById('container');
+    let Player;
+    (function (Player) {
+        class Actor {
+            buffer;
+            modifiers;
+            element;
+            keybinds;
+            using;
+            constructor(pn) {
+                this.buffer = new ArrayBuffer(4 * 1024);
+                this.modifiers = new Float16Array(this.buffer);
+                const e = document.createElement('player');
+                e.setAttribute('pn', `${pn}`);
+                e.innerHTML =
+                    `
+                    <div class="receptors">
+                        <div class="receptor left"></div>
+                        <div class="receptor down"></div>
+                        <div class="receptor up"></div>
+                        <div class="receptor right"></div>
+                    </div>
+                    <div class="notefield">
+                        <div class="line left">
+                            <tap></tap>
+                        </div>
+                        <div class="line down">
+                        </div>
+                        <div class="line up">
+                        </div>
+                        <div class="line right">
+                        </div>
+                    </div>
+                `;
+                this.element = e;
+                this.keybinds = new Uint8Array(4);
+                container.querySelector('.players').appendChild(e);
+                this.modifiers[Mods.xmod] = 1.5;
+            }
+            applyModifiers() {
+                this.element.style.transform = `
+                    rotateX(${this.modifiers[Mods.rotationx]}deg)
+                    rotateY(${this.modifiers[Mods.rotationy]}deg)
+                    rotateZ(${this.modifiers[Mods.rotationz]}deg)
+                `;
+            }
+            setKeybinds(data) {
+                this.keybinds.map((kb, i) => {
+                    return data[i];
+                });
+            }
         }
+        function create() {
+            return new Actor(document.querySelectorAll("player").length);
+        }
+        Player.create = create;
+    })(Player = Game.Player || (Game.Player = {}));
+    Game.P1 = Game.Player.create();
+    function gameLoop() {
+        Game.P1.applyModifiers();
     }
-    Players.P = [new Player, new Player];
-})(Players || (Players = {}));
-var Gamestate;
-(function (Gamestate) {
-    function export_data() {
-        const data = [];
-        FILEMANAGER.download(data.flat(1).join(''), 'gamestate.dat');
-    }
-    Gamestate.export_data = export_data;
-})(Gamestate || (Gamestate = {}));
+    setInterval(gameLoop, 1000 / 60);
+})(Game || (Game = {}));
--- src/main.ts
+++ src/main.ts
@@ -1,186 +1,93 @@
-// namespace Settings {
-//     const IDENTITY = 'rhythm/settings';
-//     let data = new ArrayBuffer(4 * 3 + 256);
-//     const U8 = new Uint8Array(data);
 
-//     const values = {
-//         volume: {
-//             master: 0,
-//             sfx: 0,
-//             background: 0, // possibly an unused property
-//             music: 0
-//         },
-//         user: {
-//             score: 0,
-//             accuracy: 0, // how accurate
-//             name: "Unnamed"
-//         }
-//     }
-    
-//     export function write()
-//     {
-//         // move all data into the array before saving to disk.
-//         // WHEN LOADING PLEASE DO NOT ORDER THIS ANY DIFFERENT.
-//         // 4 bytes for volumes.
-//         // 4(3) for [score, accuracy, u.name_len, u.name]
-
-//         // #VOLUME
-//         // a byte for each volume knob
-//         U8[0] = values.volume.master
-//         U8[1] = values.volume.sfx
-//         U8[2] = values.volume.music
-//         U8[3] = values.volume.background
-
-//         // #PROFILE
-//         const U32 = new Uint32Array(data);
-//         U32[1] = values.user.score;
-
-//         const F32 = new Float32Array(data);
-//         F32[2] = values.user.accuracy;
-        
-//         U8[12] = Math.min(values.user.name.length, 255);
-//         for (let i = 0; i < U8[12]; i++) {
-//             U8[13+i] = values.user.name.charCodeAt(i);
-//         }
-
-//         // save to disk.
-//         localStorage.setItem(IDENTITY, String.fromCodePoint(...U8));
-//     }
-
-//     // we are kinda in a rush rn, gotta push changes before class begins. -- 8/12/2025
-//     export function load()
-//     {
-//         const item = localStorage.getItem(IDENTITY);
-//         if(!item) {
-//             write();
-//             load();
-//             return;
-//         }
-
-//         item.split("").forEach((str, i) => {
-//             U8[i] = str.charCodeAt(0)
-//         })
-
-
-//         // first 4 bytes for the volumes (32 BITS)
-//         values.volume.master = U8[0];
-//         values.volume.sfx = U8[1];
-//         values.volume.music = U8[2];
-//         values.volume.background = U8[3];
-
-//         // PROFILE
-//         const U32 = new Uint32Array(data);
-//         values.user.score = U32[1];
-
-//         const F32 = new Float32Array(data);
-//         values.user.accuracy = F32[2];
-
-//         values.user.name = ""
-//         for (let i = 0; i < U8[12]; i++) {
-//             values.user.name += String.fromCharCode(U8[13+i])
-//         }
-
-//         console.log(`username: ${values.user.name} (${U8[12]})`)
-//     }
-
-//     export function getValues()
-//     {
-//         return values;
-//     }
-
-//     export function toString()
-//     {
-//         return String.fromCodePoint(...U8);
-//     }
-
-//     export function setName()
-//     {
-//         const a = prompt("Enter a memorable username.");
-//         if(!a || a.length > 255) return new Error("Could not set username.");
-//         for (let i = 0; i < a.length; i++) {
-//             const char = a.charCodeAt(i);
-//             if(
-//                 // make sure the character is within the normal ASCII thingy
-//                 (char < "0".charCodeAt(0) || char > "9".charCodeAt(0)) &&
-//                 ( char < "A".charCodeAt(0) || char > "Z".charCodeAt(0)) &&
-//                 ( char < "a".charCodeAt(0) || char > "z".charCodeAt(0))
-//             )
-//             {
-//                 return new Error("Could not set username.");
-//             }
-//         }
-//         values.user.name = a;
-//         console.log('name set to ' + a);
-//         write();
-//     }
-
-//     // load when the user loads the document
-//     document.addEventListener('DOMContentLoaded', load);
-// }
-
-
-namespace KEYBOARDMANAGER {
-
-    const keys = new Uint32Array(4);
-
-    function down(ev: KeyboardEvent)
-    {
-        ev.preventDefault();
-        console.log(ev.code)
-    }
-
-    function up(ev: KeyboardEvent)
-    {
-        ev.preventDefault();
-    }
-
-    document.addEventListener('keydown', down, {
-        passive: false,
-        capture: false,
-        once: false
-    })
-
-    document.addEventListener('keyup', up, {
-        passive: false,
-        capture: false,
-        once: false
-    })
+enum Mods {
+    xmod,
+    x, y, z,
+    movex, movey, movez,
+    rotationx, rotationy, rotationz,
+    skewx, skewy,
+    drunk, drunkspeed, drunkoffset, drunkperiod,
+    tipsy, tipsyspeed, tipsyoffset, tipsyperiod,
+    drunkz, drunkzspeed, drunkzoffset, drunkzperiod,
 }
 
-namespace FILEMANAGER {
-    export function download(fileData: string, fileName: string)
-    {
-        const e = document.createElement('a');
-        e.setAttribute('href', `data:text/plain;charset=utf-8,${fileData}`);
-        e.setAttribute('download', fileName);
-        e.style.display = 'none';
-        document.body.appendChild(e);
-        e.click();
-        e.remove();
-    }
-}
+namespace Game {
+    const container = document.getElementById('container');
+
+    export namespace Player {
 
 
+        class Actor {
+            private buffer: ArrayBuffer;
+            public modifiers: Float16Array;
+            private element: HTMLElement;
+            private keybinds: Uint8Array;
+            private using: string;
 
+            constructor(pn: number)
+            {
+                this.buffer = new ArrayBuffer(4 * 1024); // ~4k modifiers
+                this.modifiers = new Float16Array(this.buffer);
 
-namespace Players {
-    let noteWidth = 75; //px
-    let noteHeight = 75; //px
-    class Player {
-        constructor() {
+                const e = document.createElement('player');
+                e.setAttribute('pn', `${pn}`);
+                e.innerHTML =
+                `
+                    <div class="receptors">
+                        <div class="receptor left"></div>
+                        <div class="receptor down"></div>
+                        <div class="receptor up"></div>
+                        <div class="receptor right"></div>
+                    </div>
+                    <div class="notefield">
+                        <div class="line left">
+                            <tap></tap>
+                        </div>
+                        <div class="line down">
+                        </div>
+                        <div class="line up">
+                        </div>
+                        <div class="line right">
+                        </div>
+                    </div>
+                `;
+                this.element = e;
+                this.keybinds = new Uint8Array(4);
+                container.querySelector('.players').appendChild(e);
+                this.modifiers[Mods.xmod] = 1.5 ; // 1.5x DEFAULT MODIFIER SETUP
+            }
+
+            applyModifiers()
+            {
+                this.element.style.transform = `
+                    rotateX(${this.modifiers[Mods.rotationx]}deg)
+                    rotateY(${this.modifiers[Mods.rotationy]}deg)
+                    rotateZ(${this.modifiers[Mods.rotationz]}deg)
+                `
+            }
+
+            setKeybinds(data: number[]) {
+                this.keybinds.map((kb, i) => {
+                    return data[i];
+                })
+                // this.keybinds[Key.l] = data[Key.l];
+                // this.keybinds[Key.d] = data[Key.d];
+                // this.keybinds[Key.u] = data[Key.u];
+                // this.keybinds[Key.r] = data[Key.r];
+            }
+        }
+
+        export function create()
+        {
+            return new Actor(document.querySelectorAll("player").length);
         }
     }
-    export const P: Player[] = [new Player, new Player];
     
-}
+    export const P1 = Game.Player.create();
 
-namespace Gamestate {
-    export function export_data()
+    function gameLoop()
     {
-        const data: string[] = [];
-        // some other things like the game's current state and whats next in the queue, advancements, achievements idk vro you make it up.
-        // data.push(Settings.toString())
-        FILEMANAGER.download(data.flat(1).join(''), 'gamestate.dat');
+        P1.applyModifiers();
     }
-}
 
+    setInterval(gameLoop, 1000/60);
+}
